00fcfad7aaf3b09232e2524d88b5e83302f0ff89
ctorres4564 <ctorres4564@gmail.com>
2025-05-27 08:42:25 -0300

    Melhorias: CI, testes, arquivos de contribuição e ambiente

---
+++ CONTRIBUTING.md
@@ -0,0 +1,8 @@
+# Guia de Contribuição
+
+Obrigado por contribuir!
+
+- Crie um fork do repositório e faça suas alterações em uma branch separada.
+- Sempre adicione testes para novas funcionalidades.
+- Abra um Pull Request explicando suas mudanças.
+- Dúvidas? Abra uma issue!
--- config_galeria/settings.py
+++ config_galeria/settings.py
@@ -1,5 +1,3 @@
-
-
 from pathlib import Path
 import os
 
@@ -28,7 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'galeria.apps.GaleriaConfig',
+    "galeria.apps.GaleriaConfig",
 ]
 
 MIDDLEWARE = [
@@ -46,7 +44,7 @@ ROOT_URLCONF = "config_galeria.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -109,11 +107,12 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
